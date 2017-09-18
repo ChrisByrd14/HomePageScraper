be3bc965eb9eb91881fc188074bf633f772037e6
Christopher Byrd <Christopher.byrd2013@gmail.com>
2017-09-18 10:23:47 -0500

    broke system calls out to its own method
    broke system calls out to its own method

--- classes/imagegetter.py
+++ classes/imagegetter.py
@@ -7,19 +7,27 @@ from urllib.parse import urlparse
 
 class ImageGetter:
     def __init__(self, links, directory):
+        if director[-1] != '/':
+            directory += '/'
         self.dir = directory
         self.links = links
+        self.webkit_call = 'webkit2png --dir={} {}'
 
     def scrape_homepages(self):
         for link in self.links:
-            self.download(link)
-
-    def download(self, link):
-        try:
             parsed_url = urlparse(link)
             download_dir = '{}{}'.format(self.dir, parsed_url.netloc)
-            os.system('mkdir {}'.format(download_dir))
-            webkit_call = 'webkit2png --dir={} {}'.format(download_dir, link)
-            os.system(webkit_call)
+            self.make_directory(download_dir)
+            self.download(download_dir, link)
+
+    def download(self, directory, link):
+        self.system_call(self.webkit_call.format(directory, link)
+
+    def make_directory(self, directory):
+        self.system_call('mkdir {}'.format(directory))
+
+    def system_call(self, call):
+        try:
+            os.system(call)
         except Exception as e:
             print("Exception occurred: {}".format(str(e)))
